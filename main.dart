aeef77342f396e0df171690d8b710dbbca02cf7a
Natalie <nataliembatha@gmail.com>
2023-04-25 15:26:22 +0200

    OOP

--- main.dart
+++ main.dart
@@ -1,4 +1,52 @@
+// OOP
+import 'dart:io';
+
+class Book {
+  String title;
+  String author;
+  int noPages;
+
+  Book(this.title, this.author, this.noPages);
+
+  void description() {
+    print('$author wrote $title');
+  }
+}
+
+class MathQuestion {
+  String question;
+  double answer;
+
+  MathQuestion(this.question, this.answer);
+
+  double promptUser() {
+    print('Solve this: $question');
+    double userAnswer = double.parse(stdin.readLineSync()!);
+    return userAnswer;
+  }
+}
 
 void main() {
-  print('Dart is working');
+  Book bestRead = Book("Lord of the Rings", "Tolkien", 500);
+  bestRead.description();
+
+  List<MathQuestion> questions = [
+    MathQuestion('7 * 3', 21),
+    MathQuestion('12 - 5', 7),
+    MathQuestion('12.3 / 3', 4.1)
+  ];
+
+  int quizScore = 0;
+
+  for (MathQuestion question in questions) {
+    double answer = question.promptUser();
+    if (answer == question.answer) {
+      print('Correct!');
+      quizScore++;
+    } else {
+      print('Incorrect, the correct answer is ${question.answer}!');
+    }
+  }
+
+  print('You got $quizScore/${questions.length} correct');
 }
